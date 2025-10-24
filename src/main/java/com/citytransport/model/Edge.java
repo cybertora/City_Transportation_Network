6d7749c37cba0e94a4f7c4af247ac60debe6e276
rassul <rtokatov@gmail.com>
2025-10-24 19:47:31 +0500

    create constructor for json and getters/setters

--- src/main/java/com/citytransport/model/Edge.java
+++ src/main/java/com/citytransport/model/Edge.java
@@ -1,4 +1,36 @@
 package com.citytransport.model;
 
 public class Edge {
+    private String from;
+    private String to;
+    private int weight;
+
+    public Edge(){}
+
+    public Edge(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
 }
