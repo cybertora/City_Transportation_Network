8ff07dcd81a14169b6dcbd4b9ff3bb4bd3385993
rassul <rtokatov@gmail.com>
2025-10-24 19:52:58 +0500

    create constructor for JSON and getters/setters

--- src/main/java/com/citytransport/model/Graph.java
+++ src/main/java/com/citytransport/model/Graph.java
@@ -1,4 +1,39 @@
 package com.citytransport.model;
 
+import java.util.List;
+
 public class Graph {
+    private int id;
+    private List<String> nodes;
+    private List<Edge> edges;
+
+    public Graph() {}
+
+    public Graph(int id, List<String> nodes, List<Edge> edges) {
+        this.id = id;
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> getNodes() {
+        return nodes;
+    }
+    public void setNodes(List<String> nodes) {
+        this.nodes = nodes;
+    }
+
+    public List<Edge> getEdges() {
+        return edges;
+    }
+    public void setEdges(List<Edge> edges) {
+        this.edges = edges;
+    }
+
 }
