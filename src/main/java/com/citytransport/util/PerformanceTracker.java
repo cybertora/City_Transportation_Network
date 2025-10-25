de65911c388c397121bb3e46c9978c6b0e469ae1
rassul <rtokatov@gmail.com>
2025-10-25 20:52:40 +0500

    tracker

--- src/main/java/com/citytransport/util/PerformanceTracker.java
+++ src/main/java/com/citytransport/util/PerformanceTracker.java
@@ -1,4 +1,21 @@
 package com.citytransport.util;
 
 public class PerformanceTracker {
+    private int operations;
+
+    public PerformanceTracker() {
+        this.operations = 0;
+    }
+
+    public void incrementOperations(int count) {
+        this.operations += count;
+    }
+
+    public int getOperations() {
+        return operations;
+    }
+
+    public void reset() {
+        this.operations = 0;
+    }
 }
